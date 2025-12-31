0d8d212e9f4d7307fc6dc69b9d33cd7f5ba93c66
esemsc-jc4024 <jc4024@ic.ac.uk>
2025-12-31 11:22:58 +0000

    removing old files

--- old_module/legacy_calc.py
+++
@@ -1,2 +0,0 @@
-def enhanced_calc(a, b):
-    return (a + b) * 1.1
--- services/analytics.py
+++
@@ -1,4 +0,0 @@
-from utils.math_ops import compute_weighted_avg
-
-def process_scores(scores, weights):
-    return compute_weighted_avg(scores, weights)
